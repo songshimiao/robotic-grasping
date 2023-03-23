62eb273fa5f30d951d494bb1e7025669bad8bc09
songshimiao <2775270442@qq.com>
2023-03-23 22:37:15 +0800

    change camera_date and grasp_demo

--- song/camera_date.py
+++ song/camera_date.py
@@ -1,2 +1,48 @@
 import numpy as np
 import torch
+from data_processing import Image
+
+class CameraData:
+    def __init__(self, width=640, height=480, output_size=300):
+        self.output_size = output_size
+        
+        left = (width - output_size) // 2
+        top = (height - output_size) // 2
+        right = (width + output_size) // 2
+        bottom = (height + output_size) // 2
+        
+        self.top_left = (top, left)
+        self.bottom_right = (bottom, right)
+        
+        
+    @staticmethod
+    def numpy_to_torch(s):
+        if len(s.shape) == 2:
+            return torch.from_numpy(np.expand_dims(s, axis=0).astype(np.float32))
+        else:
+            return torch.from_numpy(s.astype(np.float32))
+        
+    def get_depth(self, img):
+        depth_img = Image(img)
+        depth_img.crop(self.bottom_right, self.top_left)
+        depth_img.normalise()
+        depth_img.img = depth_img.img.transpose((2, 0, 1))
+        return depth_img.img
+    
+    def get_rgb(self, img, norm=True):
+        rgb_img = Image(img)
+        rgb_img.crop(self.bottom_right, self.top_left)
+        if norm:
+            rgb_img.normalise()
+            rgb_img.img = rgb_img.img.transpose((2, 0, 1))
+        return rgb_img.img
+    
+    def get_data(self, rgb=None, depth=None):
+        rgb_img = self.get_rgb(img=rgb)
+        depth_img = self.get_depth(img=depth)
+        x = self.numpy_to_torch(
+            np.concatenate(
+                (np.expand_dims(depth_img, axis=0),
+                 np.expand_dims(rgb_img, axis=0)),
+                1))
+        return x, depth_img, rgb_img
--- song/grasp_demo.py
+++ song/grasp_demo.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from realsenseD435 import RealsenseD535
+from camera_date import CameraData
 from PIL import Image
 
 import logging
@@ -21,3 +22,26 @@ class Grasp():
         self.camera = RealsenseD535()
         self.model = None
         self.device = None
+        self.cam_data = CameraData()
+        
+        
+        
+        
+if __name__ == '__main__':
+    g = Grasp()
+    g.camera.init_cam()
+    color_image, depth_image = g.camera.get_data()
+    logging.info('color_image.shape:{}'.format(color_image.shape))
+    logging.info('depth_image.shape:{}'.format(depth_image.shape))
+    # color_image = g.cam_data.get_rgb(color_image)
+    # depth_image = g.cam_data.get_depth(depth_image)
+    # logging.info('color_image.shape:{}'.format(color_image.shape))
+    # logging.info('depth_image.shape:{}'.format(depth_image.shape))
+    x, depth_img, color_img = g.cam_data.get_data(color_image, depth_image)
+    logging.info('x.shape: {}'.format(x.shape))
+    logging.info('depth_img.shape: {}'.format(depth_img.shape))
+    logging.info('color_img.shape: {}'.format(color_img.shape))
+    
+    
+    
+    
